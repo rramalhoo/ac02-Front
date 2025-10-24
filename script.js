66125a373fc38d565c0697e69ccafd6aa0691e99
rramalhoo <raphael.athenesi@gmail.com>
2025-10-24 08:34:52 -0300

    ex03

--- script.js
+++ script.js
@@ -107,77 +107,54 @@ function mostrarResumoVendas() {
 
 
 
-
-
 //EX03
 
-const produtos = []
-const quantidades = []
-let totalProdutos = 0
-let totalItens = 0
+let produtos = []
+let quantidades = []
+let totalprod = ""
+let quantestoque = 0
+let maiorquant = 0
+let prodmf = ""
+let prodconsult = ""
+let quantconsult = 0
+let resp = ""
 
-document.getElementById('adicionar').addEventListener('click', () => {
-    const produtoInput = document.getElementById('produto').value
-    const quantidadeInput = parseInt(document.getElementById('quantidade').value)
-
-    if (produtoInput && quantidadeInput > 0) {
-        produtos.push(produtoInput)
-        quantidades.push(quantidadeInput)
-
-        totalProdutos++;
-        totalItens += quantidadeInput;
-
-        document.getElementById('produto').value = ''
-        document.getElementById('quantidade').value = ''
+function cadastrar(produto, quantidade) {
+    if (quantidade !== -1) {
+        produtos.push(produto.toLowerCase())
+        quantidades.push(quantidade)
     } else {
-        alert("Por favor, insira um produto e uma quantidade válida.")
-    }
-})
+        totalprod = produtos.length
+        quantestoque = 0
+        quantidades.forEach(quant => {
+            quantestoque += quant
+        })
 
-document.getElementById('finalizar').addEventListener('click', () => {
-    if (totalProdutos === 0) {
-        alert("Nenhum produto foi adicionado.")
-        return
-    }
-
-    const produtoMaisFrequente = encontrarProdutoMaisFrequente()
-    const quantidadeProdutoEspecifico = prompt("Digite o nome do produto que deseja consultar:")
-
-    let quantidadeEspecifica = 0
-    for (let i = 0; i < produtos.length; i++) {
-        if (produtos[i] === quantidadeProdutoEspecifico) {
-            quantidadeEspecifica += quantidades[i]
+        maiorquant = 0
+        for (let i = 0; i < quantidades.length; i++) {
+            if (quantidades[i] > maiorquant) {
+                maiorquant = quantidades[i]
+                prodmf = produtos[i]
+            }
         }
-    }
 
-    const resultados = `
-        Total de produtos inseridos: ${totalProdutos}<br>
-        Quantidade total de itens no estoque: ${totalItens}<br>
-        Produto mais frequente: ${produtoMaisFrequente.nome} (${produtoMaisFrequente.quantidade})<br>
-        Quantidade do produto "${quantidadeProdutoEspecifico}": ${quantidadeEspecifica}
-    `;
-
-    document.getElementById('resultadosEx3').innerHTML = resultados
-});
-
-function encontrarProdutoMaisFrequente() {
-    const mapaFrequencias = {}
-
-    for (let i = 0; i < produtos.length; i++) {
-        const produto = produtos[i];
-        if (!mapaFrequencias[produto]) {
-            mapaFrequencias[produto] = 0
+        prodconsult = produto.toLowerCase()
+        quantconsult = 0
+        for (let i = 0; i < produtos.length; i++) {
+            if (produtos[i] === prodconsult) {
+                quantconsult = quantidades[i]
+            }
         }
-        mapaFrequencias[produto] += quantidades[i]
+
+        resp = `
+            <h2>Resumo dos Produtos</h2>
+            <p>
+                <b>Total de produtos cadastrados:</b> ${totalprod} | 
+                <b>Quantidade total em estoque:</b> ${quantestoque} | 
+                <b>Produto mais frequente:</b> ${prodmf} | 
+                <b>Produto específico (${prodconsult}):</b> Quantidade ${quantconsult}
+            </p>
+        `
+        document.getElementById("resultado").innerHTML = resp
     }
-
-    let produtoMaisFrequente = { nome: '', quantidade: 0 }
-
-    for (const produto in mapaFrequencias) {
-        if (mapaFrequencias[produto] > produtoMaisFrequente.quantidade) {
-            produtoMaisFrequente = { nome: produto, quantidade: mapaFrequencias[produto] };
-        }
-    }
-
-    return produtoMaisFrequente
 }
